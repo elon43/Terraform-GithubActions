1012460a7b88ca47700c738f78e3dd934754a6ed
Stefan Thompson <elon43@yahoo.com>
2023-05-15 03:14:15 +0000

    terraform apply works for stg/vpc and stg/ec2

--- dev/main.tf
+++ dev/main.tf
@@ -2,19 +2,18 @@ module "vpc" {
   source = "../modules/vpc"
 
   CIDR_BLOCK       = var.CIDR_BLOCK
-#  vpc_id            = module.vpc.vpc_id
   TENANCY           = var.TENANCY
   CIDR_BLOCK_SUBNET = var.CIDR_BLOCK_SUBNET
   NAME              = var.NAME
 }
 
-/*
+
 module "ec2" {
   source       = "../modules/ec2"
-  ami          = var.AMI_ID
-  instance_typ = var.INSTANCE_TYPE
-  subnet_id    = var.SUBNET_ID
-  name         = var.NAME
-  region       = var.REGION
+
+  AMI_ID          = var.AMI_ID
+  INSTANCE_TYPE = var.INSTANCE_TYPE
+  SUBNET_ID    = module.vpc.subnet_id
+  NAME         = var.NAME
+  REGION      = var.REGION
 }
-*/
